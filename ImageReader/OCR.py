2723bd8c9d59736daaf8515437200c62122a652e
Suhas <suhasbs.nitk@gmail.com>
2017-09-01 16:14:39 +0530

    bootstrap modal

--- ImageReader/OCR.py
+++ ImageReader/OCR.py
@@ -1,7 +1,24 @@
 import subprocess
 import models
 from OCRWebApp import settings
+import cv2
+
+def checkResolution(path):
+
+	img = cv2.imread(path)
+	if(img.shape[0]<500 and img.shape[1]<500):
+		width = 500
+		height = float(width)/img.shape[0]*img.shape[1]
+		res_img = cv2.resize(img, (int(width), int(height)))
+		cv2.imwrite(path, res_img)
+		print 'resized'
+
+
+
 def convertImageToText(path, imgid):
+
+    checkResolution(path)
+
     a = models.ImageUploads.objects.filter(id=imgid)[0]
     a.status = 2
     a.save()
